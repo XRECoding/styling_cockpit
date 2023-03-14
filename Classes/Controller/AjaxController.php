45e5438d849cdb2f927d55819b1407346ddda800
Henri Welsch <s4jowels@uni-trier.de>
2023-03-14 15:12:55 +0100

    adding old features

--- Classes/Controller/AjaxController.php
+++ Classes/Controller/AjaxController.php
@@ -19,8 +19,35 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Resource\ResourceFactory;
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+// use TYPO3\CMS\Core\Utility\GeneralUtility;
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
+
+
+
+
+use TYPO3\CMS\Core\Context\Context;
+
+
+
+
+ExtensionManagementUtility::addPageTSConfig('
+    @import "EXT:styling_cockpit/Configuration/page.tsconfig"
+');
+
+
+
+
+
+// echo "<pre>";
+// print_r(BackendUtility::getPagesTSconfig(0));
+
+
+// echo "</pre>";
+
+
+
 
 /**
  * AjaxController
@@ -42,67 +69,132 @@ class AjaxController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 
         // ******************************************************
 
-        $test = "homepage1";
-        $homepageArray = array();
-        $gridArray = array();
-
-        $homepageOptions = array();
-        $gridOptions = array();
-
-
 
         $layouts = BackendUtility::getPagesTSconfig(1)["mod."]["web_layout."]["BackendLayouts."];
 
+        $homepageOptions = [];
+        $gridOptions = [];
+
         foreach($layouts as $key => $value) {
-            // echo explode(".", $key)[0]."<br>";
-            $keyus = explode(".", $key)[0];
-
-            $testLayout = "<div id='".$keyus."' style='visibility: collapse; padding: 0;'>";
-            $heightCounter = count($value["config."]["backend_layout."]["rows."]) -2;
-
-            if (!str_contains($key, "homepage")) {
-                $testLayout .= "<div id='".$keyus."_header' name='header' onclick='alert();' style='height: 20%; width:100%; border: 1px solid black; background-image: linear-gradient(to bottom right,  transparent calc(50% - 1px), black, transparent calc(50% + 1px));'>header</div>";
-                $heightCounter += 2;
-                array_push($gridOptions, $keyus);
+            if (str_contains($key, "homepage")) {
+                array_push($homepageOptions, explode(".", $key)[0]);
             } else {
-                array_push($homepageOptions, $keyus);
+                array_push($gridOptions, explode(".", $key)[0]);
             }
-
-            foreach ($value["config."]["backend_layout."]["rows."] as $layout) {
-                $mar = explode("-", $key);
-                $zahl = 1;
-
-                foreach ($layout["columns."] as $sub) {
-                    $a = (count($layout["columns."]) !== 1) ? 'display: inline-block;' : '';
-                    $b = (count($mar) == 1) ? 1 : ((int)$mar[$zahl++]) / 100;
-                    $c = (str_contains($key, "homepage")) ? 1 / count($layout["columns."]) : $b;
-
-
-                    $testKilian = end(explode(".", $sub['name']));
-
-                    if ($testKilian == "header") {
-                        $testLayout .= "<div id='".$keyus."_header' name='header' onclick='onClick(this);' style='height: 20%; width:100%; border: 1px solid black'>header</div>";
-                    } else if ($testKilian == "footer") {
-                        $testLayout .= "<div id='".$keyus."footer' name='footer' onclick='onClick(this);' style='height: 20%; width:100%; border: 1px solid black'>footer</div>";
-                    } else {
-                        $testLayout .= "<div id='".$keyus."_".$testKilian."' name=".$keyus."_".$testKilian." onclick='onClick(this);' style='height:". 60 / $heightCounter."%;width:". 100 * $c ."%; border: 1px solid black;".$a."'>".$testKilian."</div>";
-                    }
-                }
-
-            }
-
-            if (!str_contains($key, "homepage")) {
-                $testLayout .= "<div id='".$keyus."_footer' name='footer' onclick='alert();' style='height: 20%; width:100%; border: 1px solid black; background-image: linear-gradient(to bottom right,  transparent calc(50% - 1px), black, transparent calc(50% + 1px));'>footer</div>";
-            }
-            $testLayout .= "</div>";
-
-            if (!str_contains($key, "homepage")) {
-                array_push($gridArray, $testLayout);
-            } else {
-                array_push($homepageArray, $testLayout);
-            }
-
         }
+        
+
+
+        $homepageArray = [];
+        $gridArray = [];
+
+        foreach ($homepageOptions as $homepage) {
+            foreach($layouts as $key => $value) {
+                $layoutName = explode(".", $key)[0];
+                $homepageID = (!strcmp($layoutName, $homepage)) ? $layoutName : $layoutName."_".$homepage;
+   
+                $layoutContainer = "<div id='".$homepageID."' style='visibility: collapse; padding: 0;'>";
+                $heightCounter = count($value["config."]["backend_layout."]["rows."]) -2;
+    
+                if (!str_contains($key, "homepage")) {
+                    $layoutContainer .= "<div id='".$layoutName."_header' name='header_".$homepage."' onclick='alert();' style='height: 20%; width:100%; border: 1px solid black; background-image: linear-gradient(to bottom right,  transparent calc(50% - 1px), black, transparent calc(50% + 1px));'>header</div>";
+                    $heightCounter += 2;
+                } 
+    
+                foreach ($value["config."]["backend_layout."]["rows."] as $layout) {
+                    $mar = explode("-", $key);
+                    $zahl = 1;
+    
+                    foreach ($layout["columns."] as $sub) {
+                        $a = (count($layout["columns."]) !== 1) ? 'display: inline-block;' : '';
+                        $b = (count($mar) == 1) ? 1 : ((int)$mar[$zahl++]) / 100;
+                        $c = (str_contains($key, "homepage")) ? 1 / count($layout["columns."]) : $b;
+    
+    
+                        $gridName = end(explode(".", $sub['name']));
+    
+                        if ($gridName == "header") {
+                            $layoutContainer .= "<div id='".$layoutName."_header' name='header_".$homepage."' onclick='onClick(this);' style='height: 20%; width:100%; border: 1px solid black'>header</div>";
+                        } else if ($gridName == "footer") {
+                            $layoutContainer .= "<div id='".$layoutName."footer' name='footer_".$homepage."' onclick='onClick(this);' style='height: 20%; width:100%; border: 1px solid black'>footer</div>";
+                        } else {
+                            $layoutContainer .= "<div id='".$layoutName."_".$gridName."' name=".$layoutName."_".$gridName." onclick='onClick(this);' style='height:". 60 / $heightCounter."%;width:". 100 * $c ."%; border: 1px solid black;".$a."'>".$gridName."</div>";
+                        }
+                    }
+    
+                }
+    
+                if (!str_contains($key, "homepage")) {
+                    $layoutContainer .= "<div id='".$layoutName."_footer' name='footer_".$homepage."' onclick='alert();' style='height: 20%; width:100%; border: 1px solid black; background-image: linear-gradient(to bottom right,  transparent calc(50% - 1px), black, transparent calc(50% + 1px));'>footer</div>";
+                }
+    
+                if (!str_contains($key, "homepage")) {
+                    array_push($gridArray, $layoutContainer.="</div>");
+                } else {
+                    array_push($homepageArray, $layoutContainer.="</div>");
+                }
+            }
+        }
+        
+        // Returns the current page ID
+        // $pageId = (int) GeneralUtility::_GET('id');
+
+
+        // echo (int)\TYPO3\CMS\Backend\Utility\BackendUtility::BEgetRootLine(1)[0]['uid'];
+
+        // $pid = intVal($_GET['id']);
+        // $page = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
+        // echo $page;
+
+        // $rootPageId = BackendUtility::getPagesTSconfig(1)['TCEFORM.']['pages.'];
+
+
+        // $pid = intVal($_GET['id']);
+        // $page = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
+        // $root = array_pop($page->getRootLine($pid));
+
+        // require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('frontend') . 'Classes/Page/PageRepository.php');
+
+        // $pid = intVal($_GET['id']);
+
+        // // get current root page record
+        // $pageRepositoryObj = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
+        // $rootPageRecord = $pageRepositoryObj->getRootLine($pid);
+
+        // // assign the first element of the returned array to the variable
+        // $rootPageId = array_pop($rootPageRecord)['uid'];
+
+        // echo $rootPageId;''
+        // echo (int)\TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('http://localhost/studienprojekt/news');
+
+        // echo (int)$GLOBALS['TSFE']->rootLine[0]['uid'];
+        // echo strval($GLOBALS['TSFE']->rootLine[0]['title']);
+
+        
+        $pid = intVal($_GET['id']);
+        $page = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Domain\\Repository\\PageRepository');
+
+        // $root = $page->getRootLine($pid);
+
+        // echo $root;
+
+        // echo "<pre>";
+        // print_r($page);
+    
+    
+        // echo "</pre>";
+
+
+        // echo $page->getRootLine($pid);
+        // $root = array_pop($page->getRootLine($pid));
+
+        // $pageId = (int) GeneralUtility::_GET('id');
+        // $rootPageId = BackendUtility::BEgetRootLine($pageId)[0]['uid'];
+        // echo $rootPageId;
+
+
+
+
         $this->view->assign("homepageArray", $homepageArray);
         $this->view->assign("gridArray", $gridArray);
         $this->view->assign("homepageOptions", $homepageOptions);
@@ -111,28 +203,10 @@ class AjaxController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 
 
         // ******************************************************
-        // getting the background color for div elements
-        // TODO change filepath from absolute to relative
-        // header
-        $fileString = file_get_contents("D://Dokumente/XAMPP/htdocs/Studienprojekt/typo3conf/ext/styling_cockpit/Resources/Public/Css/header.css");
-        $headerStart = strpos($fileString, "div.site-header");
-        $colorSelector = strpos($fileString, "background-color:", $headerStart);
-        $colorStart = strpos($fileString, " ", $colorSelector)+1;
-        $colorEnd = strpos($fileString, ";", $colorStart);
-
-        $headerColor = substr($fileString, $colorStart, $colorEnd - $colorStart);
-
-        //footer
-        $fileString = file_get_contents("D://Dokumente/XAMPP/htdocs/Studienprojekt/typo3conf/ext/styling_cockpit/Resources/Public/Css/footer.css");
-        $footerStart = strpos($fileString, "div.site-footer");
-        $colorSelector = strpos($fileString, "background-color:", $footerStart);
-        $colorStart = strpos($fileString, " ", $colorSelector)+1;
-        $colorEnd = strpos($fileString, ";", $colorStart);
-
-        $footerColor = substr($fileString, $colorStart, $colorEnd - $colorStart);
 
         return $this->htmlResponse();
     }
+
 
     public function doSomethingAction(ServerRequestInterface $request): ResponseInterface
     {
@@ -157,55 +231,16 @@ class AjaxController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
         fclose($file);
         */
 
-        /*
-        $storageRepository = GeneralUtility::makeInstance(StorageRepository::class);
-        $defaultStorage = $storageRepository->getDefaultStorage();
-        $folder = $defaultStorage->getFolder('EXT:styling_cockpit/resources/public/css');
-        $file = $folder->getStorage()->getFileInFolder("test.txt", $folder);
-
-        fwrite($file,"test");
-        fclose($file);
-        */
 
         // works but cant use absolute path
         /*
-        $file = fopen("D://Dokumente/XAMPP/htdocs/Studienprojekt/typo3conf/ext/styling_cockpit/Resources/Public/Css/header.css", "r");
+        $file = fopen("D://Dokumente/XAMPP/htdocs/Studienprojekt/typo3conf/ext/styling_cockpit/Resources/Public/Css/test.txt", "w");
         fwrite($file, "test");
         fclose($file);
         */
 
-        // TODO change filepath from absolute to relative
-        //edit header background color
-        $fileString = file_get_contents("D://Dokumente/XAMPP/htdocs/Studienprojekt/typo3conf/ext/styling_cockpit/Resources/Public/Css/header.css");
-        $headerStart = strpos($fileString, "div.site-header");
-        $colorSelector = strpos($fileString, "background-color:", $headerStart);
-        $colorStart = strpos($fileString, " ", $colorSelector)+1;
-        $colorEnd = strpos($fileString, ";", $colorStart);
 
-        $file = fopen("D://Dokumente/XAMPP/htdocs/Studienprojekt/typo3conf/ext/styling_cockpit/Resources/Public/Css/test.txt", "w");
-        fwrite($file, substr($fileString, 0, $colorStart));
-        fwrite($file, "color");     // TODO
-        fwrite($file, substr($fileString, $colorEnd));
-        fclose($file);
-
-        //edit footer background color
-        $fileString = file_get_contents("D://Dokumente/XAMPP/htdocs/Studienprojekt/typo3conf/ext/styling_cockpit/Resources/Public/Css/footer.css");
-        $footerStart = strpos($fileString, "div.site-footer");
-        $colorSelector = strpos($fileString, "background-color:", $footerStart);
-        $colorStart = strpos($fileString, " ", $colorSelector)+1;
-        $colorEnd = strpos($fileString, ";", $colorStart);
-
-        $file = fopen("D://Dokumente/XAMPP/htdocs/Studienprojekt/typo3conf/ext/styling_cockpit/Resources/Public/Css/test.txt", "w");
-        fwrite($file, substr($fileString, 0, $colorStart));
-        fwrite($file, "color");     // TODO
-        fwrite($file, substr($fileString, $colorEnd));
-        fclose($file);
-
-
-
-
-
-        $data = ['result' => 'pls work'];
+        $data = ['result' => 'my stuff'];
         return $this->jsonResponse(json_encode($data));
     }
 }
--- Resources/Public/JavaScript/Backend.js
+++ Resources/Public/JavaScript/Backend.js
@@ -17,15 +17,17 @@ document.getElementById("color-picker").addEventListener("change", function(even
 
 
 function onClick(div) {
-    if (div.getAttribute("name") == "header") {
-        var elms = document.querySelectorAll("[name='header']");
+    var currentHomepage = getCurrentHomepage();
+
+    if (div.getAttribute("name") == "header_" + currentHomepage) {
+        var elms = document.querySelectorAll("[name='header_"+currentHomepage+"']");
         for (var i = 0; i < elms.length; i++) 
-            elms[i].style.backgroundColor=colorPickerValue;
+            elms[i].style.backgroundColor = colorPickerValue;
 
     } else if (div.getAttribute("name") == "footer") {
-        var elms = document.querySelectorAll("[name='footer']");
+        var elms = document.querySelectorAll("[name='footer_"+currentHomepage+"']");
         for (var i = 0; i < elms.length; i++) 
-            elms[i].style.backgroundColor=colorPickerValue;
+            elms[i].style.backgroundColor = colorPickerValue;
             
     } else {
         div.style.backgroundColor = colorPickerValue; 
@@ -42,8 +44,7 @@ function alertColor(){
 }
 
 function changeHomepage() {
-    var e = document.getElementById("homepageOption");
-    var currentHomepage = e.options[e.selectedIndex].text;
+    var currentHomepage = getCurrentHomepage();
 
     if (lastHomepage != "") {
         let a = document.getElementById(lastHomepage);
@@ -53,11 +54,13 @@ function changeHomepage() {
     let p = document.getElementById(currentHomepage);
     p.style.visibility = "visible";
     lastHomepage = currentHomepage;
+
+    changeGrid();
 }
 
 function changeGrid() {
     var e = document.getElementById("gridOption");
-    var currentGrid = e.options[e.selectedIndex].text;
+    var currentGrid = e.options[e.selectedIndex].text + "_" + getCurrentHomepage();
 
     if (lastGrid != "") {
         let a = document.getElementById(lastGrid);
@@ -69,21 +72,13 @@ function changeGrid() {
     lastGrid = currentGrid;
 }
 
-require(['TYPO3/CMS/Core/Ajax/AjaxRequest'], function (AjaxRequest) {
-    var saveButton = document.getElementsByClassName("testButton");
-    let writeCSS = function (element){
-        new AjaxRequest(TYPO3.settings.ajaxUrls.stylingcockpit_dosomething)
-            .withQueryArguments({input: 1})
-            .get()
-            .then(async function (response) {
-                const resolved = await response.resolve();
-                console.log(resolved.result);
-            });
-    }
+function getCurrentHomepage() {
+    var a = document.getElementById("homepageOption");
+    return a.options[a.selectedIndex].text;
+}
 
-    for (let i = 0; i < saveButton.length; i++) {
-        saveButton[i].addEventListener('click', writeCSS, false);
-    }
-
-
-});
+function changeFont() {
+    var e = document.getElementById("fontOption");
+    var selectedFont = e.options[e.selectedIndex].text;
+    console.log(selectedFont);
+}
